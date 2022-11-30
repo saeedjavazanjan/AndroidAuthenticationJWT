7d5900586145b8160fa2c120e85ca4b29edab368
saeed <ghfarisaeed1@gmail.com>
2022-11-30 18:42:42 +0330

    finish baase

--- androidjwt/src/main/java/com/saeed/authentication/androidjwt/JWT.kt
+++ androidjwt/src/main/java/com/saeed/authentication/androidjwt/JWT.kt
@@ -3,12 +3,14 @@ package com.saeed.authentication.androidjwt
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.saeed.authentication.androidjwt.auth.*
 import dagger.Provides
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
@@ -19,19 +21,23 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
-class JWT(baseUrl:String,var condition:Condition ) {
+class JWT(baseUrl:String,var condition:Condition ,context: Context) {
  var repository:AuthRepository
-init {
-this.repository= authRepository(AuthApi(baseUrl),sharedPref(Application()))
-    authenticate()
+    lateinit var state :AuthState
+
+    init {
+this.repository= authRepository(AuthApi(baseUrl),sharedPref(context))
+
 
     CoroutineScope(Dispatchers.Main).launch{
         resultEvents()
     }
+        this.state= AuthState()
+    authenticate()
 
 }
 
-    var state by mutableStateOf(AuthState())
+
 
     private val resultChannel = Channel<AuthResult<Unit>>()
     val authResults = resultChannel.receiveAsFlow()
@@ -45,6 +51,8 @@ this.repository= authRepository(AuthApi(baseUrl),sharedPref(Application()))
                 password = state.signUpPassword
             )
             resultChannel.send(result)
+            resultEvents()
+
             state = state.copy(isLoading = false)
         }
     }
@@ -57,6 +65,8 @@ this.repository= authRepository(AuthApi(baseUrl),sharedPref(Application()))
                 password = state.signInPassword
             )
             resultChannel.send(result)
+                resultEvents()
+
             state = state.copy(isLoading = false)
         }
     }
@@ -66,22 +76,27 @@ this.repository= authRepository(AuthApi(baseUrl),sharedPref(Application()))
             state = state.copy(isLoading = true)
             val result = repository.authenticate()
             resultChannel.send(result)
+            resultEvents()
+
             state = state.copy(isLoading = false)
         }
     }
 
     suspend fun  resultEvents(){
-        authResults.collect { result ->
-            when(result) {
-                is AuthResult.Authorized -> {
-                   condition.Authorized()
-                }
-                is AuthResult.Unauthorized -> {
-                    condition.Unauthorized()
+        CoroutineScope(Dispatchers.Main).launch {
 
-                }
-                is AuthResult.UnknownError -> {
-                     condition.UnknownError()
+            authResults.collect { result ->
+                when (result) {
+                    is AuthResult.Authorized -> {
+                        condition.Authorized()
+                    }
+                    is AuthResult.Unauthorized -> {
+                        condition.Unauthorized()
+
+                    }
+                    is AuthResult.UnknownError -> {
+                        condition.UnknownError()
+                    }
                 }
             }
         }
@@ -95,8 +110,9 @@ this.repository= authRepository(AuthApi(baseUrl),sharedPref(Application()))
             .create()
     }
 
-   private fun sharedPref(app: Application): SharedPreferences {
-        return app.getSharedPreferences("prefs", Context.MODE_PRIVATE)
+   private fun sharedPref(context: Context): SharedPreferences {
+       val sharedPreference=context.getSharedPreferences("prefs",Context.MODE_PRIVATE)
+        return sharedPreference
     }
 
    private fun authRepository(api: AuthApi, prefs: SharedPreferences): AuthRepository {
